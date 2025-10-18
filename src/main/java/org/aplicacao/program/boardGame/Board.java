96dc93c2bdb8472d1e9e9357245b62d153be76ed
tempestaGui <guilhermeftempesta@gmail.com>
2025-10-18 16:25:05 -0300

    Implement defensive programming in Board method

--- src/main/java/org/aplicacao/program/boardGame/Board.java
+++ src/main/java/org/aplicacao/program/boardGame/Board.java
@@ -1,11 +1,16 @@
 package org.aplicacao.program.boardGame;
 
+import org.aplicacao.program.boardGame.exceptions.BoardException;
+
 public class Board {
     private int rows;
     private int columns;
     private Piece[][] pieces;
 
     public Board(int rows, int columns){
+        if(rows < 1 || columns < 1){
+            throw new BoardException("Rows and columns must be greater than 1");
+        }
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
@@ -28,14 +33,23 @@ public class Board {
     }
 
     public Piece piece(int row, int column){
+        if(!positionExists(row, column)){
+            throw new BoardException("Position does not exist");
+        }
         return pieces[row][column];
     }
 
     public Piece piece(Position position){
+        if(!positionExists(position)){
+            throw new BoardException("Position does not exist");
+        }
         return pieces[position.getRow()][position.getColumn()]; //retornando a peca que esta na posicao da linha e da coluna
     }
 
     public void placePiece(Piece piece, Position position){ //vai na matriz de pecas e atribuir a peca que vir como argumento
+        if(thereIsAPiece(position)){
+            throw new BoardException("Piece already exist in this position "+position);
+        }
         pieces[position.getRow()][position.getColumn()] =  piece;
         piece.position = position;
     }
@@ -49,6 +63,9 @@ public class Board {
     }
 
     public boolean thereIsAPiece(Position position){
+        if(!positionExists(position)){
+            throw new BoardException("Position does not exist");
+        }
         return piece(position) != null;
     }
 
