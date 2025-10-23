f4be2d488c775bd11bb9298640effe15a2fb98b6
tempestaGui <guilhermeftempesta@gmail.com>
2025-10-23 20:52:19 -0300

    added color code

--- src/main/java/org/aplicacao/program/application/UI.java
+++ src/main/java/org/aplicacao/program/application/UI.java
@@ -1,8 +1,33 @@
 package org.aplicacao.program.application;
 
 import org.aplicacao.program.chess.ChessPiece;
+import org.aplicacao.program.chess.Color;
 
 public class UI {
+
+
+    //codigo especias das cores para imprimir no console (Cores do texto)
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    //codigo especias das cores para imprimir no console (Cores do fundo)
+    public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+    public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+    public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
+
     public static void printBoard(ChessPiece[][] pieces) {
         for(int i = 0; i < pieces.length; i++) {
             System.out.print((8 - i) + " "); //imprimir o rotulo lateral de 1 a 8
@@ -19,7 +44,11 @@ public class UI {
             System.out.print("-");
         }
         else {
-            System.out.print(piece);
+            if(piece.getColor() == Color.white){
+                System.out.println(ANSI_WHITE + piece + ANSI_RESET);
+            } else{
+                System.out.println(ANSI_RED + piece + ANSI_RESET);
+            }
         }
         System.out.print(" "); // para nao ficar colado
     }
